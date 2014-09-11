421e7e57386a6b51cfebb6c4134f19bdbae9f66a
Rueben Ramirez <ruebenramirez@gmail.com>
2014-09-11 12:30:26 -0400

    fixing blog and adding an article

--- runDocker.sh
+++ runDocker.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
+set +e
+
+WORKING_DIR=`pwd`
+
+docker build -t ruebenramirez/blog.ruebenramirez.com .
+
+# docker push ruebenramirez/blog.ruebenramirez.com
+
+docker rm -f blog.ruebenramirez.com
+
 docker run -d \
     -p 80:80 \
-    -v ./nginx /etc/nginx/sites-enabled/ \
-    -v ./content /var/www/ \
+    -v $WORKING_DIR/output/:/var/www/ \
+    -v $WORKING_DIR/nginx:/etc/nginx/sites-enabled/ \
     --name blog.ruebenramirez.com \
-    nginx 
+    ruebenramirez/blog.ruebenramirez.com
+
